4f2ed6dd9f22f1214f6bd23b06d1cae7597d29f2
Davit Nahapetyan <dnahapet@gmail.com>
2023-10-17 17:51:08 -0400

    Created FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Davit Nahapetyan on 2023-10-17.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
