09696b77fcc46c43cfece7ec01ff3c2b9f0e7029
Davit Nahapetyan <dnahapet@gmail.com>
2023-10-20 20:54:23 -0400

    Created factory method to create system under test.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -15,7 +15,7 @@ class RemoteFeedLoader {
     var httpClient: HTTPClient
     var httpURL: URL
 
-    init(client: HTTPClient, url: URL = URL(string: "http://a-url.com")!) {
+    init(client: HTTPClient, url: URL) {
         httpClient = client
         httpURL = url
     }
@@ -25,29 +25,35 @@ class RemoteFeedLoader {
     }
 }
 
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesnotRequestURL() {
+        let (client, _) = makeSUT()
+
+        XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_withRequestURL() {
+        let (client, sut) = makeSUT(url: URL(string: "http://given-url.com")!)
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
+    }
+}
+
+//MARK: - Helpers
+
+func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (HTTPClientSpy, RemoteFeedLoader) {
+    let client = HTTPClientSpy()
+    let sut = RemoteFeedLoader(client: client, url: url)
+    return (client, sut)
+}
+
 class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
 
     func get(from url: URL) {
         requestedURL = url
-    }
-}
-
-class RemoteFeedLoaderTests: XCTestCase {
-
-    func test_init_doesnotRequestURL() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
-        
-        XCTAssertNil(client.requestedURL)
-    }
-
-    func test_load_withRequestURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
-
-        sut.load()
-
-        XCTAssertNotNil(client.requestedURL)
     }
 }
