01306b93dbe4ae20ea2554facf3028571443ab1a
Davit Nahapetyan <dnahapet@gmail.com>
2023-10-27 10:47:00 -0400

    Made FeedItem not depend on implementation details such as API load key path.

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CloudKit
 
 public enum HTTPClientResult {
     case success(Data, HTTPURLResponse)
@@ -43,7 +44,7 @@ public final class RemoteFeedLoader {
             case .success(let data, _):
                 do {
                     let root = try JSONDecoder().decode(Root.self, from: data)
-                    completion(.success(root.items))
+                    completion(.success(root.items.map({ $0.feedItem })))
                 } catch {
                     completion(.failure(.invalidData))
                 }
@@ -53,5 +54,16 @@ public final class RemoteFeedLoader {
 }
 
 private struct Root: Decodable {
-    var items: [FeedItem]
+    let items: [Item]
+}
+
+private struct Item: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+
+    var feedItem: FeedItem {
+        return FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
 }
