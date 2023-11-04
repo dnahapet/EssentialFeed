0b74ddaece26d38244a8ef4f7cbf4edf0c2e661e
Davit Nahapetyan <dnahapet@gmail.com>
2023-11-04 11:27:46 -0400

    Resumes DataTask with URL

--- EssentialFeed/EssentialFeedTests/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -16,9 +16,7 @@ class URLSessionHTTPClient {
     }
 
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in
-            
-        }
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
@@ -26,24 +24,51 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     func test_getFromURL_createsDataTaskWithURL() {
         let url = URL(string: "http://some-url.com")!
-        let session = URLSessionSpy()
+        let task = FakeURLSessionDataTask()
+        let session = URLSessionSpy(with: task)
 
         let sut = URLSessionHTTPClient(session)
         sut.get(from:url)
 
         XCTAssertEqual(session.requestedURLs, [url])
     }
+    
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "http://some-url.com")!
+        let task = URLSessionDataTaskSpy()
+        let session = URLSessionSpy(with: task)
+
+        let sut = URLSessionHTTPClient(session)
+        sut.get(from:url)
+
+        XCTAssertEqual(task.resumeCount, 1)
+    }
+    
+    // MARK: - Helpers
 
     private class URLSessionSpy: URLSession {
         var requestedURLs = [URL]()
+        private let dataTask: URLSessionDataTask
+
+        init(with dataTask:URLSessionDataTask) {
+            self.dataTask = dataTask
+        }
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             requestedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return dataTask
         }
     }
 
     private class FakeURLSessionDataTask:URLSessionDataTask {
-        
+        override func resume() {}
+    }
+    
+    private class URLSessionDataTaskSpy:URLSessionDataTask {
+        var resumeCount: Int = 0
+
+        override func resume() {
+            resumeCount += 1
+        }
     }
 }
