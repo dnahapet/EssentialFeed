c6bde86588babbab8726619284dd736a59b458b2
Davit Nahapetyan <dnahapet@gmail.com>
2023-11-29 11:52:36 -0500

    Deletes local cache upon saving new items.

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFromCacheUseCaseTests.swift
@@ -6,22 +6,52 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
     let store: FeedStore
     init(store: FeedStore) {
         self.store = store
     }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCache()
+    }
 }
 
 class FeedStore {
-    let deletedCacheCount = 0
+    var deletedCacheCallCount = 0
+
+    func deleteCache() {
+        deletedCacheCallCount += 1
+    }
 }
 
 class LoadFromCacheUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
-        XCTAssertEqual(store.deletedCacheCount, 0)
+        XCTAssertEqual(store.deletedCacheCallCount, 0)
+    }
+    
+    func test_save_DeletesCache() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+
+        let items = [uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deletedCacheCallCount, 1)
+    }
+
+    //MARK: Helpers
+
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: nil, location: nil, imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
