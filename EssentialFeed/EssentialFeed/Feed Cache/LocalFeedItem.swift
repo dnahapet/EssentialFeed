86c1085253eb12d1e6841da3f2b753c183ba20e5
Davit Nahapetyan <dnahapet@gmail.com>
2023-12-04 13:53:20 -0500

    Moved LocalFeedItem to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Davit Nahapetyan on 2023-12-04.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
