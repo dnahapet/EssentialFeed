8ecd483412bd20a575bfdf8c13ba47b8f792db02
Davit Nahapetyan <dnahapet@gmail.com>
2023-12-13 12:31:08 -0500

    ValidateCache deletes cache upon cache retrieval error

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -39,7 +39,6 @@ public final class LocalFeedLoader {
 
             switch result {
             case let .failure(error):
-                self.store.deleteCache() { _ in }
                 completion(.failure(error))
 
             case let .found(localFeedImages, timestamp) where self.validateTimestamp(timestamp):
@@ -53,6 +52,11 @@ public final class LocalFeedLoader {
                 completion(.success([]))
             }
         }
+    }
+
+    public func validateCache() {
+        store.retrieve() { _ in }
+        store.deleteCache() { _ in }
     }
 
     private var maxCacheAgeInDays: Int {
