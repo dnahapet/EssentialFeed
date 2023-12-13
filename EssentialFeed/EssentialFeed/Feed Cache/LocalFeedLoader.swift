bc20b23a2523858287a1f356ad31f9353704b660
Davit Nahapetyan <dnahapet@gmail.com>
2023-12-13 13:13:15 -0500

    Group switch cases which does the same thing

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -44,10 +44,7 @@ public final class LocalFeedLoader {
             case let .found(localFeedImages, timestamp) where self.validateTimestamp(timestamp):
                 completion(.success(localFeedImages.toModels()))
 
-            case .found:
-                completion(.success([]))
-
-            case .empty:
+            case .found, .empty:
                 completion(.success([]))
             }
         }
