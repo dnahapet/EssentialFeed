bc39a0fb7a9637c61cb39263cac556f8b19c1b93
Davit Nahapetyan <dnahapet@gmail.com>
2024-01-09 19:03:19 -0500

    Extract system under test SUT into helper method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -74,7 +74,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
 
         let exp = expectation(description: "Wait for cache retrieval completion")
 
@@ -93,7 +93,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
 
         let exp = expectation(description: "Wait for cache retrieval completion")
 
@@ -114,7 +114,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueFeed().localModels
         let timestamp = Date()
         let exp = expectation(description: "Wait for cache retrieval completion")
@@ -136,5 +136,11 @@ class CodableFeedStoreTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 1.0)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> CodableFeedStore {
+        return CodableFeedStore()
     }
 }
