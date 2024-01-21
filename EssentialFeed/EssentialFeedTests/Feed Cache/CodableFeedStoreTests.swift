5b36f661b174f7a82653b86c350581866788d0bc
Davit Nahapetyan <dnahapet@gmail.com>
2024-01-21 18:52:30 -0500

    Extracted FeedStore tests into separate protocols

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,37 +8,8 @@
 import XCTest
 import EssentialFeed
 
-protocol FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnEmptyCache()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+class CodableFeedStoreTests: XCTestCase, FeedStoreSpecs, FailableFeedStoreSpecs {
 
-    func test_insert_overridesPreviouslyInsertedCacheValues()
-
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()
-
-    func test_storeSideEffects_runSerially()
-}
-
-protocol FailableRetrieveOperationSpecs: FeedStoreSpecs {
-    func test_retrieve_deliversFailureOnRetrievalError()
-    func test_retrieve_hasNoSideEffectsOnRetrievalError()
-}
-
-protocol FailableInsertOperationSpecs: FeedStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-    func test_insert_deliversNoErrorOnNonEmptyCache()
-}
-
-protocol FailableDeleteOperationSpecs: FeedStoreSpecs {
-    func test_delete_deliversFailureOnDeletionError()
-}
-
-class CodableFeedStoreTests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
 
