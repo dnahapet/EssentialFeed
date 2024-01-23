ed68576c460c9e61dd31580bb4d14cc2b857cb47
Davit Nahapetyan <dnahapet@gmail.com>
2024-01-23 12:32:12 -0500

    Extract model translations into helper methods within the managed models

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage+CoreDataProperties.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/Core Data/ManagedFeedImage+CoreDataProperties.swift
@@ -19,6 +19,21 @@ extension ManagedFeedImage {
     @NSManaged public var location: String?
     @NSManaged public var url: URL
     @NSManaged public var cache: ManagedCache
+
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
+
+    var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
 }
 
 extension ManagedFeedImage : Identifiable {
