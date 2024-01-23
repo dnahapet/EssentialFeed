0063f3f03e2a50d1625767df2cf9e8d62dd4cc27
Davit Nahapetyan <dnahapet@gmail.com>
2024-01-23 16:59:02 -0500

    Used CodableFeedStore in cache integration tests

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -55,8 +55,9 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         let storeURL = testSpecificStoreURL()
-        let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+//        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+//        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let store = CodableFeedStore(storeURL: storeURL)
         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
