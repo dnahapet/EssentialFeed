93c5af00114e206dbc0937640e348ee826862f85
Davit Nahapetyan <dnahapet@gmail.com>
2024-01-23 15:46:51 -0500

    Add 'EssentialFeedCacheIntegrationTests' target to separate cache integration to tests from unit/isolation tests.

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Davit Nahapetyan on 2024-01-23.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
