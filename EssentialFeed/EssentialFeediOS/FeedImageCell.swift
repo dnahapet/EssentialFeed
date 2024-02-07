3a28210797d3b48ecf64a71746e6154cc4fc068f
Davit Nahapetyan <dnahapet@gmail.com>
2024-02-07 11:10:12 -0500

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Davit Nahapetyan on 2024-02-07.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public var locationContainer: UIView = UIView()
+    public var locationLabel: UILabel = UILabel()
+    public var descriptionLabel: UILabel = UILabel()
+}
