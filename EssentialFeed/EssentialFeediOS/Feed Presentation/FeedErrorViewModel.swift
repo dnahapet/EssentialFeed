cb43c35c344e48544aac1b6710c20e9ba76a6db1
Davit Nahapetyan <dnahapet@gmail.com>
2024-03-11 16:46:04 -0400

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Davit Nahapetyan on 2024-03-11.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
