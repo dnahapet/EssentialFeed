106e54a572f736691e84c2e6935db96c3e556de7
Davit Nahapetyan <dnahapet@gmail.com>
2024-04-07 19:21:35 -0400

    Added empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Davit Nahapetyan on 2024-04-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
