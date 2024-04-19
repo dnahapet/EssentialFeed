883c9da82d4a61940c4fba533c4bc090c25abf1f
Davit Nahapetyan <dnahapet@gmail.com>
2024-04-19 15:57:43 -0400

    Replace FeedLoader composition with Combine operators

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -5,23 +5,24 @@
 //  Created by Davit Nahapetyan on 2024-02-12.
 //
 
-import Foundation
 import UIKit
+import Combine
 import EssentialFeed
 import EssentialFeediOS
 
 public final class FeedUIComposer {
     private init() {}
 
-    public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
-
+    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: FeedImageDataLoader) -> FeedViewController {
+        let presentationAdapter = FeedLoaderPresentationAdapter(
+            feedLoader: { feedLoader().dispatchOnMainQueue() })
+        
         let feedController = makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
-
+        
         presentationAdapter.presenter = FeedPresenter(
             feedView: FeedViewAdapter(controller: feedController, imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
             loadingView: WeakRefVirtualProxy(feedController),
-             errorView: WeakRefVirtualProxy(feedController))
+            errorView: WeakRefVirtualProxy(feedController))
         return feedController
     }
 
