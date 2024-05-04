dd64afbfd9971bfca369261d7e690968361d7e93
Davit Nahapetyan <dnahapet@gmail.com>
2024-05-04 12:55:38 -0400

    Replace composition of multiple protocols with single type

---
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/CellController.swift
@@ -0,0 +1,26 @@
+//
+//  CellController.swift
+//  EssentialFeediOS
+//
+//  Created by Davit Nahapetyan on 2024-05-04.
+//
+
+import UIKit
+
+public struct CellController {
+    let dataSource: UITableViewDataSource
+    let delegate: UITableViewDelegate?
+    let dataSourcePrefetching: UITableViewDataSourcePrefetching?
+    
+    public init(_ dataSource: (UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching)) {
+        self.dataSource = dataSource
+        self.delegate = dataSource
+        self.dataSourcePrefetching = dataSource
+    }
+    
+    public init(_ dataSource: UITableViewDataSource) {
+        self.dataSource = dataSource
+        self.delegate = nil
+        self.dataSourcePrefetching = nil
+    }
+}
